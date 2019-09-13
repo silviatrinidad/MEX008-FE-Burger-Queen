d54a7ec0f2783c3eae957b672e9c4cc51ffdbc1b
anabahena <ana-bahena@live.com.mx>
2019-09-13 15:30:38 -0500

    Agregando maquetacion de pantalla de mesas

--- burguer-queen/src/app/app.module.ts
+++ burguer-queen/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import 'hammerjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { PruebaComponent } from './prueba/prueba.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { LateralBarComponent } from './lateral-bar/lateral-bar.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+
 
 @NgModule({
+  exports:[MatGridListModule],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PruebaComponent,
+    LateralBarComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ burguer-queen/src/app/prueba/prueba.component.ts
@@ -0,0 +1,22 @@
+import {Component} from '@angular/core';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
+/**
+ * @title Dynamic grid-list
+ */
+@Component({
+  selector: 'app-prueba',
+  templateUrl: 'prueba.component.html',
+  styleUrls: ['prueba.component.css'],
+})
+export class PruebaComponent {
+  tiles: Tile[] = [
+
+  ];
+}
