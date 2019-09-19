ada04a1b05accaa49dede10c0e68e1a5e2deda68
Rebeca Chiñas <chi.san.rebeca@gmail.com>
2019-09-19 00:11:12 -0500

    Agregando angular router y el componente que contiene a la pagina principal y la comanda

---
+++ burguer-queen/src/app/take-order/take-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-take-order',
+  templateUrl: './take-order.component.html',
+  styleUrls: ['./take-order.component.css']
+})
+export class TakeOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
