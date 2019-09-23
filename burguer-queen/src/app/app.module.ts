c9aa1db5c67cd9699d049079933a4aa0a3ba0f09
Daynizyvc <zinyad_leay@hotmail.com>
2019-09-22 20:54:00 -0500

    Cambios agregados al poner el boton que indica el estatus de la orden

--- burguer-queen/src/app/app.module.ts
+++ burguer-queen/src/app/app.module.ts
@@ -16,11 +16,6 @@ import {MatListModule} from '@angular/material/list';
 // Componentes que contienen la pagina de las mesas-barras y la comanda respectivamente
 import { InitContainerComponent } from './init-container/init-container.component';
 import { TakeOrderComponent } from './take-order/take-order.component';
-<<<<<<< HEAD
-import { MenuComponent } from './menu/menu.component';
-import { Component, OnInit } from '@angular/core';
-// import { Prueba2Component } from './prueba2/prueba2.component'
-=======
 import { BillComponent } from './bill/bill.component';
 import { BillClientComponent } from './bill-client/bill-client.component';
 import { MenuComponent} from './menu/menu.component';
@@ -33,12 +28,13 @@ import { BarTotalComponent } from './bar-total/bar-total.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatRadioModule} from '@angular/material/radio';
 import { ModalPayCashComponent } from './bill-client/bill-client.component'
-import {MatDialogModule} from '@angular/material/dialog'; 
+// import {MatDialogModule} from '@angular/material/dialog'; 
 import {MatGridListModule} from '@angular/material/grid-list';
->>>>>>> e4e75bccf07e7733f6bcee02d06c7211dfba0572
 
 //Yael
-import { MatDialogModule } from '@angular/material';
+import { StateOrdenComponent } from './state-orden/state-orden.component'
+import { MatDialogModule, MatBadgeModule } from '@angular/material';
+import { DialogStatusOrdenComponent } from './dialog-status-orden/dialog-status-orden.component';
 //
 
 
@@ -56,7 +52,8 @@ import { MatDialogModule } from '@angular/material';
     BarTotalComponent,
     MenuComponent,
     ModalPayCashComponent,
-  
+    StateOrdenComponent,
+    DialogStatusOrdenComponent, 
   
   ],
   imports: [
@@ -74,8 +71,9 @@ import { MatDialogModule } from '@angular/material';
     MatCardModule,
     MatRadioModule,
     MatDialogModule,
-    MatGridListModule
-    
+    MatGridListModule,
+    //Yael
+    MatBadgeModule    
   ],
   providers: [
    
--- burguer-queen/src/app/state-orden/state-orden.component.ts
+++ burguer-queen/src/app/state-orden/state-orden.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { DialogStatusOrdenComponent } from '../dialog-status-orden/dialog-status-orden.component'
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-state-orden',
   templateUrl: './state-orden.component.html',
   styleUrls: ['./state-orden.component.css']
 })
-export class StateOrdenComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class StateOrdenComponent {
+  tiles: Tile[] = [
+    {color: '', cols: 3, rows: 1, text: ''} 
+  ];
+  constructor(public dialog: MatDialog) {}
+  openDialog(){
+    this.dialog.open(DialogStatusOrdenComponent);
   }
-
 }
