1397c5d73f8d375185e5eb20b04206ae86a58893
Daynizyvc <zinyad_leay@hotmail.com>
2019-10-03 15:54:12 -0500

    Se agrega el modal de la hamburguesa

---
+++ burguer-queen/src/app/modal-hamburger-soberbia/modal-hamburger-soberbia.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-modal-hamburger-soberbia',
+  templateUrl: './modal-hamburger-soberbia.component.html',
+  styleUrls: ['./modal-hamburger-soberbia.component.css']
+})
+export class ModalHamburgerSoberbiaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
